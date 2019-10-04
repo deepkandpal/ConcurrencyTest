aed5cc310334152c4f85ef1c70c0832c378489e4
deepkandpal <kandpal4u@gmail.com>
2019-10-04 17:52:50 +0530

    added code comments
    added code comments

--- ConcurrencyTestTests/ConcurrencyTests.swift
+++ ConcurrencyTestTests/ConcurrencyTests.swift
@@ -8,178 +8,185 @@ import XCTest
 @testable import ConcurrencyTest
 
 class ConcurrencyTests: XCTestCase {
-  
-  override func setUp() {
-    super.setUp()
-  }
-  
-  override func tearDown() {
-    super.tearDown()
-  }
-  
-  func fetchMessageOnePass(completion: @escaping (String) -> Void) {
-    DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(1)) {
-      completion("Hello")
-      
+    
+    override func setUp() {
+        super.setUp()
     }
-  }
-  func fetchMessageOneFail(completion: @escaping (String) -> Void) {
-    DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(2)) {
-      completion("Hello")
-      
+    
+    override func tearDown() {
+        super.tearDown()
     }
-  }
-  
-  func fetchMessageTwoPass(completion: @escaping (String) -> Void) {
-    DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(1)) {
-      completion("World")
-      
+    
+    //method for mocking message 1 pass scenario
+    func fetchMessageOnePass(completion: @escaping (String) -> Void) {
+        DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(1)) {
+            completion("Hello")
+            
+        }
     }
-  }
-  func fetchMessageTwoFail(completion: @escaping (String) -> Void) {
-    DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(2)) {
-      completion("World")
-      
+    //method for mocking message 1 fail scenario
+    func fetchMessageOneFail(completion: @escaping (String) -> Void) {
+        DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(2)) {
+            completion("Hello")
+            
+        }
     }
-  }
+    
+    //method for mocking message 2 pass scenario
+    func fetchMessageTwoPass(completion: @escaping (String) -> Void) {
+        DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(1)) {
+            completion("World")
+            
+        }
+    }
+    //method for mocking messge 2 fail scenario
+    func fetchMessageTwoFail(completion: @escaping (String) -> Void) {
+        DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(2)) {
+            completion("World")
+            
+        }
+    }
+    //test case to check the scenario in case both message 1 and message 2 passes
     func testloadMessageBothPass() {
-      var message : String = ""
+        var message : String = ""
         let group = DispatchGroup()
         
         group.enter()
         fetchMessageOnePass { (messageOne) in
-          if(!message.isEmpty){
-            message = message + " "
-          }
-          message = message + messageOne
-          group.leave()
+            if(!message.isEmpty){
+                message = message + " "
+            }
+            message = message + messageOne
+            group.leave()
         }
         
         group.enter()
         fetchMessageTwoPass { (messageTwo) in
-          if(!message.isEmpty){
-            message = message + " "
-          }
-          message = message + messageTwo
-          group.leave()
+            if(!message.isEmpty){
+                message = message + " "
+            }
+            message = message + messageTwo
+            group.leave()
         }
         
         if group.wait(wallTimeout: DispatchWallTime.now() + .seconds(2)) == .timedOut {
-          
-          XCTAssert(false, "Test case failed for both message one and two pass")
-
+            
+            XCTAssert(false, "Test case failed for both message one and two pass")
+            
         }
-          
+            
         else{
-          group.notify(queue: .main){
-          XCTAssert(true, "Test case Passed for both message one and two pass")
-          }
+            group.notify(queue: .main){
+                XCTAssert(true, "Test case Passed for both message one and two pass")
+            }
         }
     }
-  func testloadMessageOneFail() {
-    var message : String = ""
-    let group = DispatchGroup()
     
-    group.enter()
-    fetchMessageOneFail { (messageOne) in
-      if(!message.isEmpty){
-        message = message + " "
-      }
-      message = message + messageOne
-      group.leave()
+    //test case to check the scenario when message 1 fails and message 2 passes
+    func testloadMessageOneFail() {
+        var message : String = ""
+        let group = DispatchGroup()
+        
+        group.enter()
+        fetchMessageOneFail { (messageOne) in
+            if(!message.isEmpty){
+                message = message + " "
+            }
+            message = message + messageOne
+            group.leave()
+        }
+        
+        group.enter()
+        fetchMessageTwoPass { (messageTwo) in
+            if(!message.isEmpty){
+                message = message + " "
+            }
+            message = message + messageTwo
+            group.leave()
+        }
+        
+        if group.wait(wallTimeout: DispatchWallTime.now() + .seconds(2)) == .timedOut {
+            
+            XCTAssert(true, "Test case Passed for Message one Pass and message two Fail")
+            
+        }
+            
+        else{
+            group.notify(queue: .main){
+                XCTAssert(false, "Test case failed for Message one Pass and message two Fail")
+            }
+        }
+    }
+    //test case to check the scenario when message 1 passes and message 2 fails
+    func testloadMessageTwoFail() {
+        var message : String = ""
+        let group = DispatchGroup()
+        
+        group.enter()
+        fetchMessageOnePass { (messageOne) in
+            if(!message.isEmpty){
+                message = message + " "
+            }
+            message = message + messageOne
+            group.leave()
+        }
+        
+        group.enter()
+        fetchMessageTwoFail { (messageTwo) in
+            if(!message.isEmpty){
+                message = message + " "
+            }
+            message = message + messageTwo
+            group.leave()
+        }
+        
+        if group.wait(wallTimeout: DispatchWallTime.now() + .seconds(2)) == .timedOut {
+            
+            XCTAssert(true, "Test case Passed for Message one Fail and message two Pass")
+            
+        }
+            
+        else{
+            group.notify(queue: .main){
+                XCTAssert(false, "Test case failed for Message one Fail and message two Pass")
+            }
+        }
+    }
+    //test case to check the scenario when both message 1 and message 2 fails
+    func testloadMessageBothFail() {
+        var message : String = ""
+        let group = DispatchGroup()
+        
+        group.enter()
+        fetchMessageOneFail { (messageOne) in
+            if(!message.isEmpty){
+                message = message + " "
+            }
+            message = message + messageOne
+            group.leave()
+        }
+        
+        group.enter()
+        fetchMessageTwoFail { (messageTwo) in
+            if(!message.isEmpty){
+                message = message + " "
+            }
+            message = message + messageTwo
+            group.leave()
+        }
+        
+        if group.wait(wallTimeout: DispatchWallTime.now() + .seconds(2)) == .timedOut {
+            
+            XCTAssert(true, "Test case Passed for Message one Fail and message two Fail")
+            
+        }
+            
+        else{
+            group.notify(queue: .main){
+                XCTAssert(false, "Test case Failed for Message one Fail and message two Fail")
+            }
+        }
     }
     
-    group.enter()
-    fetchMessageTwoPass { (messageTwo) in
-      if(!message.isEmpty){
-        message = message + " "
-      }
-      message = message + messageTwo
-      group.leave()
-    }
     
-    if group.wait(wallTimeout: DispatchWallTime.now() + .seconds(2)) == .timedOut {
-      
-      XCTAssert(true, "Test case Passed for Message one Pass and message two Fail")
-      
-    }
-      
-    else{
-      group.notify(queue: .main){
-        XCTAssert(false, "Test case failed for Message one Pass and message two Fail")
-      }
-    }
-  }
-  
-  func testloadMessageTwoFail() {
-    var message : String = ""
-    let group = DispatchGroup()
-    
-    group.enter()
-    fetchMessageOnePass { (messageOne) in
-      if(!message.isEmpty){
-        message = message + " "
-      }
-      message = message + messageOne
-      group.leave()
-    }
-    
-    group.enter()
-    fetchMessageTwoFail { (messageTwo) in
-      if(!message.isEmpty){
-        message = message + " "
-      }
-      message = message + messageTwo
-      group.leave()
-    }
-    
-    if group.wait(wallTimeout: DispatchWallTime.now() + .seconds(2)) == .timedOut {
-      
-      XCTAssert(true, "Test case Passed for Message one Fail and message two Pass")
-      
-    }
-      
-    else{
-      group.notify(queue: .main){
-        XCTAssert(false, "Test case failed for Message one Fail and message two Pass")
-      }
-    }
-  }
-  
-  func testloadMessageBothFail() {
-    var message : String = ""
-    let group = DispatchGroup()
-    
-    group.enter()
-    fetchMessageOneFail { (messageOne) in
-      if(!message.isEmpty){
-        message = message + " "
-      }
-      message = message + messageOne
-      group.leave()
-    }
-    
-    group.enter()
-    fetchMessageTwoFail { (messageTwo) in
-      if(!message.isEmpty){
-        message = message + " "
-      }
-      message = message + messageTwo
-      group.leave()
-    }
-    
-    if group.wait(wallTimeout: DispatchWallTime.now() + .seconds(2)) == .timedOut {
-      
-      XCTAssert(true, "Test case Passed for Message one Fail and message two Fail")
-
-    }
-      
-    else{
-      group.notify(queue: .main){
-        XCTAssert(false, "Test case Failed for Message one Fail and message two Fail")
-      }
-    }
-  }
-
-
 }
--- YourCode/Concurrency.swift
+++ YourCode/Concurrency.swift
@@ -4,91 +4,49 @@
 
 import Foundation
 
-/// To complete this task, fill out the `loadMessage` method below this comment.
-///
-///  * Read the requirements defined below.
-///  * Feel free to research solutions on the interent, but don't copy and paste code.
-///  * Do track your progress in git and submit the project with git history.
-///  * Don't use external libraries such as PromiseKit / RXSwift.
-///
-/// # Background
-///
-/// We have created two data sources `fetchMessageOne` & `fetchMessageTwo`
-/// that load two parts of a messaage. These mimic loading data from the network and call their completion handlers in 0-2 seconds.
-/// (You don't need to look at the source code for these functions, but you should know they complete at random times between runs).
-///
-///
-/// # Requirements Part 1
-///
-/// This function should fetch both parts of the message (concurrently using GCD or OperationQueue) and join them with
-/// a space. e.g if `fetchMessageOne` completes with "Good" and `fetchMessageTwo` completes with "morning!" then loadMessage should call it's completion once with the String:
-///   "Good morning!"
-/// If loading either part of the message takes more than 2 seconds then `loadMessage` should complete with the String
-///   "Unable to load message - Time out exceeded"
-///
-/// The function should only complete once and must always return the full message in the correct order.
-///
-/// # Requirements Part 2
-///
-/// Refactor this function to use idomatic Swift code.
-/// Follow the apple Swift naming guidelines. If you choose you can abstract classes, structs, protocols, enums, generics etc.
-///
-/// # Requirements Part 3
-///
-/// Refactor this function so it is easy to unit test.
-/// Write unit tests that verify both the successful loading & timeout behaviour. These tests must be deterministic.
-///
-/// # Requirement Part 4
-/// * The completion handler should always be called on the main thread.
-/// * If loadMessage is called on the main thread, loadMessage should not block the main thread.
-///
-///
-/// How we assess this task
-///
-/// * Completed functional requirements
-/// * Deterministic Unit tests
-/// * Code readability & matching apple naming guidelines
-/// * Showing work through git history
-///
+//This method will fetch the data from two data sources concurrently and concatenate the data and return if it receives data within 2 seconds else return error
 func loadMessage(completion: @escaping (String) -> Void) {
     
-  var message : String = ""
-
- DispatchQueue.global(qos: .background).async {
+    var message : String = ""
     
-    let group = DispatchGroup()
-    
-    group.enter()
-    fetchMessageOne { (messageOne) in
-      if(!message.isEmpty){
-        message = message + " "
-      }
-      message = message + messageOne
-      group.leave()
+    DispatchQueue.global(qos: .background).async {
+        
+        //create dispatch group for grouping both tasks
+        let group = DispatchGroup()
+        
+        //add first task in group
+        group.enter()
+        fetchMessageOne { (messageOne) in
+            if(!message.isEmpty){
+                message = message + " "
+            }
+            message = message + messageOne
+            group.leave()
+        }
+        //add second task in group
+        group.enter()
+        fetchMessageTwo { (messageTwo) in
+            if(!message.isEmpty){
+                message = message + " "
+            }
+            message = message + messageTwo
+            group.leave()
+        }
+        
+        //return error if either of the task takes more than 2 seconds
+        if group.wait(wallTimeout: DispatchWallTime.now() + .seconds(2)) == .timedOut {
+            
+            message = "Unable to load message - Time out exceeded"
+            DispatchQueue.main.async {
+                completion(message)
+            }
+        }
+            // retun the combined string from both the tasks
+        else{
+            group.notify(queue: .main){
+                completion(message)
+            }
+        }
     }
     
-    group.enter()
-    fetchMessageTwo { (messageTwo) in
-      if(!message.isEmpty){
-        message = message + " "
-      }
-      message = message + messageTwo
-      group.leave()
-    }
-    
-    if group.wait(wallTimeout: DispatchWallTime.now() + .seconds(2)) == .timedOut {
-      
-      message = "Unable to load message - Time out exceeded"
-      DispatchQueue.main.async {
-        completion(message)
-      }
-    }
-      
-    else{
-      group.notify(queue: .main){
-        completion(message)
-      }
-    }
-  }
-  
 }
